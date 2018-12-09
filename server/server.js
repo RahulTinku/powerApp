f0a917f4d3fa9d9e3df3758f927c18d4f639f6f4
Rahul Kumar <rahulkumar271@gmail.com>
2018-12-09 20:25:46 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongojs = require('mongojs');
 const path = require('path');
+
+const jsforce = require('jsforce');
+const session = require('express-session');
+const util = require('util');
+
 var users = require('./routes/user');
 var index = require('./routes/index');
+var salesforce = require('./routes/salesforce');
 
 const app = express();
 
@@ -18,31 +24,24 @@ app.use(function(req,res,next) {
 	next();
 });
 
+//initialize session
+app.use(session({secret: 'S3CRE7', resave: true, saveUninitialized: true}));
+
+var accessToken;
+var instanceUrl;
+var refreshToken;
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.engine('html', require('ejs').renderFile);
-//app.use(express.static(__dirname + '../client/src'));
 
 app.set('powersupport', db);
 
-// app.use('/', function(req, res) {
-// 	res.send("application running on port: " + port);
-// });
-
-// var jsforce = require('jsforce');
-// var conn = new jsforce.Connection();
-// conn.login('rahul.kumar@powerschool.com.emsdev', 'Tinku_3456', function(err, res) {
-//   if (err) { return console.error(err); }
-//   else { console.log(res);}
-//   // conn.query('SELECT Id, Name FROM Account', function(err, res) {
-//   //   if (err) { return console.error(err); }
-//   //   console.log(res);
-//   // });
-// });
-
+app.use('/', salesforce);
 app.use('/api', users);
-app.use('/', index); 
+//app.use('/', index); 
 
 // app.get('*', function(req, res) {
 // 	res.send('hello');
